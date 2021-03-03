18ec5ff2220abe7790fef2a2169ffd9c01dae274
Daniel Villegas <d.villegas@visiontravel.net>
2021-03-02 22:47:01 -0500

    auto-resize textarea and save note functionality fixed

--- app.js
+++ app.js
@@ -1,21 +1,32 @@
 const notes = [];
-const form = document.querySelector('form')
+const form = document.querySelector('form');
+const autoResizeElements = document.querySelectorAll('.auto-resize')
 
-const saveNote = (event) => {
-    // prevent page refresh on form submit
-    event.preventDefault();
-
-    // create a note object
-    const note = {
+// Here we create the note data
+const createNoteData = () => {
+    const noteData = {
         id: Date.now(),
         title: document.querySelector('#new-note-title').value,
         content: document.querySelector('#new-note-content').value
     }
+
+    return noteData;
+}
+
+// Here we extract the note data
+// Therefore, saveNote() does not need to receive the form data but rather call createNoteData()
+// Thus, we can use it to create eventListeners that don't need the form data passed to it, since the data gets created AFTER the event trigger. 
+const saveNote = (event) => {
+    // // prevent page refresh on form submit
+    event.preventDefault();
+    // save noteData to note variable
+    const note = createNoteData();
     // add note to notes array
     notes.push(note);
-    console.log(notes);
     // set notes array to local storage
-    window.localStorage.setItem('notes', JSON.stringify(notes));
+    if ( localStorage.getItem('notes') !== null ) {    
+        localStorage.setItem( 'notes', JSON.stringify(notes) );
+    }   
     // clear the form for new entries
     form.reset();
     console.log('form submitted')
@@ -27,14 +38,23 @@ form.addEventListener('submit', saveNote)
 
 // Submit form with keyboard shortcut: metaKey + enter
 document.addEventListener('keydown', (event) => {
-    
-    // if ( event.metaKey && event.key === 'Enter' ) {
-    //     // PROBLEM: FORM DATA IS NOT BEING PASSED IN, LOCAL STORAGE THUS NOT UPDATED
-    //     form.submit();
-    //     // document.querySelector('#save-note-btn').click()
-    // }
-   
+    if ( event.ctrlKey && event.key === 'Enter' ) {
+        document.querySelector('#save-note-btn').click()        
+    } 
 })
 
-console.log(notes)
-console.log(window.localStorage)
+// -----------------------------
+// UX details
+
+// This allows the textarea to automatically resize instead of remaining the same height with a scroll bar
+autoResizeElements.forEach(element => {
+    element.addEventListener('input', autoResizeHeight)
+})
+
+function autoResizeHeight() {
+  this.style.height = "auto";
+  this.style.height = this.scrollHeight + "px";
+  this.style.borderColor = "green";
+}
+
+// -----------------------------
